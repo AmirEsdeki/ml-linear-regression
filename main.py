04c7ce3b9c307b13f8824b9e5f6a3184393d4c38
Amir Esdeki <amir.esdeki@livecricketdata.com>
2026-04-30 23:30:58 +0100

    fix namings

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ path = kagglehub.dataset_download("nalisha/job-salary-prediction-dataset")
 print("Path to dataset files:", path)
 
 
-df = pd.read_csv(path + "/job_salary_prediction_dataset.csv")
+df_train = pd.read_csv(path + "/job_salary_prediction_dataset.csv")
 
 #take 100 of data for test reasons
-df_test = df.sample(1000, random_state=42)
-df = df.drop(df_test.index)
+df_test = df_train.sample(1000, random_state=42)
+df_train = df_train.drop(df_test.index)
 
-input_features_x = df.iloc[:,:-1].values
-output_targets_y = df.iloc[:,-1:].values.flatten() #.flatten to have a ndarray of shape (m,) instead of (m,1)
+input_features_x = df_train.iloc[:,:-1].values
+output_targets_y = df_train.iloc[:,-1:].values.flatten() #.flatten to have a ndarray of shape (m,) instead of (m,1)
 
 
